6445632402ae1beb76524e1e0783b4523c8f4b7b
Anton Lopatuxin <loparuxin.a@gmail.com>
2024-05-22 20:45:17 +0300

    Добавил логирование при инициализации бота

--- src/main/java/rf/lopatuxin/tgbot/config/BotInitializer.java
+++ src/main/java/rf/lopatuxin/tgbot/config/BotInitializer.java
@@ -1,6 +1,7 @@
 package rf.lopatuxin.tgbot.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import rf.lopatuxin.tgbot.service.TelegramBot;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class BotInitializer {
 
@@ -20,8 +22,9 @@ public class BotInitializer {
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(bot);
+            log.info("Телеграмм бот успешно зарегистрирован");
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            log.error("При регистрации бота произошла ошибка " + e.getMessage());
         }
     }
 }
