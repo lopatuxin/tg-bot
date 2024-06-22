bf60946d700ea2df6c67473fb2df8d773c95edfc
Антон <101828305+lopatuxin@users.noreply.github.com>
2024-06-22 22:46:43 +0300

    Исправил баг, когда Liquibase при каждом запуске бота пытался создавать таблицу

--- src/main/java/rf/lopatuxin/tgbot/config/DataLoader.java
+++ src/main/java/rf/lopatuxin/tgbot/config/DataLoader.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import rf.lopatuxin.tgbot.service.VideoService;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Component
@@ -15,8 +17,14 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        videoService.saveVideo("Моя жизнь", Paths.get("src/main/resources/videos/My life.mp4"));
-        videoService.saveVideo("Как легко зарабатывать", Paths.get("src/main/resources/videos/money.mp4"));
-        videoService.saveVideo("Работа в интернете", Paths.get("src/main/resources/videos/about_your_work.mp4"));
+        saveVideoIfNotExist("Моя жизнь", Paths.get("src/main/resources/videos/My life.mp4"));
+        saveVideoIfNotExist("Как легко зарабатывать", Paths.get("src/main/resources/videos/money.mp4"));
+        saveVideoIfNotExist("Работа в интернете", Paths.get("src/main/resources/videos/about_your_work.mp4"));
+    }
+
+    private void saveVideoIfNotExist(String name, Path path) throws IOException {
+        if (videoService.getVideo(name).isEmpty()) {
+            videoService.saveVideo(name, path);
+        }
     }
 }
